fde6c344488df9357134585354f34f7211af420a
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2025-03-04 14:36:58 -0300

    feat: adiciona hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import logo from '../assets/imagens/logo-verde-medio.svg';
 
 export function Header() {
     return (
-        <header className="fixed z-50 w-screen px-4 md:px-16 lg:px-28 py-4">
+        <header className="fixed z-50 w-screen px-4 md:px-16 lg:px-28 h-20 justify-between items-center flex py-4 bg-white">
             <div className="flex container mx-auto py-3 justify-between">
                 <a href="/" className="self-center">
                     <img src={logo} alt="Site logo" className="h-8 md:h-10" />
@@ -32,7 +32,7 @@ export function Header() {
                     />
                 </div>
 
-                <div className="flex items-center gap-2 md:gap-4">
+                <div className="flex items-center gap-2 md:gap-4 relative">
                     <a href="/login" className="">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -49,10 +49,11 @@ export function Header() {
                             />
                         </svg>
                     </a>
-                    <a href="/cart" className="">
+                    <a href="/cart" className="static px-1">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
+                            // fill="var(--green-medium)"
                             viewBox="0 0 24 24"
                             stroke-width="1.5"
                             stroke="currentColor"
@@ -64,6 +65,9 @@ export function Header() {
                                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
                             />
                         </svg>
+                        {/* <div className="absolute bg-(--green-medium) right-0 text-xs px-1 rounded-full top-0 md:top-0.5 text-white">
+                            5
+                        </div> */}
                     </a>
                 </div>
             </div>
